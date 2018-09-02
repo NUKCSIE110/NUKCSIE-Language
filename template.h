187283de285575cae3eb46e0294df4cd48581255
Kai-Lun Huang <kevin7776382@gmail.com>
2018-09-02 09:30:27 +0800

    Update and rename template.cpp to template.h

--- template.h
+++ template.h
@@ -1,7 +1,12 @@
 #include<cstdio>
 #include<cstdlib>
+#include<cmath>
 #include<algorithm>
-
+#include<vector>
+#include<list>
+#include<queue>
+#include<vector>
+#include<stack>
 #define 好阿 ;
 #define 茂勛哩 return 0;
 #define 系主任萬歲 int main()
